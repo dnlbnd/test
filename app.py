8bfe1c592763f001fc3f567bd8e699201af63642
nikhiljsk <nikhiljsk98@gmail.com>
2020-08-21 23:57:18 +0530

    intial

---
+++ app.py
@@ -0,0 +1,16 @@
+import os
+import json
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return json.dumps({"message": "Hello there!"})
+
+@app.route('/hello')
+def index2():
+    return json.dumps({"message": "Pani chestundi ani cheppa ga...nammava. Dobbey."})
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
