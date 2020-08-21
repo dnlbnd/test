3cc68c321485cc8755e87b30a338797708f800a8
nikhiljsk <nikhiljsk98@gmail.com>
2020-08-22 01:06:29 +0530

    check

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return json.dumps({"message": "Hello there you go man2lasf as!"})
+    return json.dumps({"message": "Total changed"})
 
 @app.route('/hello')
 def index2():
