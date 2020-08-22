f4b38492b6306f204365073caacb6d5946ca94cd
nikhiljsk <nikhiljsk98@gmail.com>
2020-08-22 15:54:46 +0530

    add2

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_cors import CORS, cross_origin
 
@@ -11,4 +12,4 @@ def helloWorld():
   return "Hello, cross-origin-world!"
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
+    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8088)))
