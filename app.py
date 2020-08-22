e9ad5759add176eac5d3bf63674959c5a772b2b2
nikhiljsk <nikhiljsk98@gmail.com>
2020-08-22 15:59:19 +0530

    add3

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 import os
+import json
 from flask import Flask
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
+
+@app.route("/hello")
+@cross_origin()
+def helloWorld():
+  return json.loads({"message": "ANIL!!"})
 
 @app.route("/")
 @cross_origin()
